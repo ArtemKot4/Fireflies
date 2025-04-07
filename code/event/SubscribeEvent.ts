822001ab210933b738901ca706651580b3697cf6
ArtemKot4 <artemon4xxl@gmail.com>
2025-04-07 06:49:30 +0300

    Некоторые изменения
    Теперь SubscribeEvent может быть как декоратором, так и фабрикой декораторов.
    
    Если просто навесить декоратор над методом, он добавит событие исходя из названия функции.
    В прочем, есть примеры с дебага:
    
    class Example1 {
        @SubscribeEvent(ECallback.LOCAL_TICK)
        public tipaTick() {
            if(World.getThreadTime() % 20 === 0) Game.message("example 1 with one arg")
        };
    };
    
    class Example2 {
        @SubscribeEvent
        public onLocalTick() {
            if(World.getThreadTime() % 20 === 0) Game.message("example 2 without args")
        };
    };

--- code/event/SubscribeEvent.ts
+++ code/event/SubscribeEvent.ts
@@ -8,13 +8,44 @@
             Game.message("example")
         }
     };
+
+    
  * ```
  * @param event {@link ECallback} enum value
  * @returns decorator
  */
-function SubscribeEvent(event: ECallback) {
-    return function(target: any, key: string, descriptor: PropertyDescriptor) {
-        Callback.addCallback(event, descriptor.value);
-        return descriptor;
+function SubscribeEvent(event: ECallback): MethodDecorator;
+
+/**
+ * Decorator to add callback from function by function name and same function. Format will be "onNameOfCallback". "on" optional.
+ * @example
+ * ```ts
+ * class ExampleDestroyBlock {
+        SubscribeEvent
+        public onDestroyBlock() {
+            Game.message("break block")
+        }
+    }
+ * ```
+ * @param target 
+ * @param key 
+ * @param descriptor 
+ */
+function SubscribeEvent(target: unknown, key: string, descriptor: PropertyDescriptor): PropertyDescriptor;
+
+function SubscribeEvent(value: unknown, key?: string, descriptor?: PropertyDescriptor): unknown {
+    if(typeof value === "string" && arguments.length === 1) {
+        return function(target: any, key: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+            Callback.addCallback(value, descriptor.value);
+            return descriptor;
+        };
     };
+    let name = key.replace("on", "");
+
+    if(name === "Tick") {
+        name = "tick";
+    };
+
+    Callback.addCallback(name, descriptor.value);
+    return descriptor;
 };
